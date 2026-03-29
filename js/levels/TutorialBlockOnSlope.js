83028ffa447b8eecbbc2b008c249fac50b523543
udinyang <147449694+udinyang@users.noreply.github.com>
2026-03-28 23:59:53 -0400

    changed friction value

--- js/levels/TutorialBlockOnSlope.js
+++ js/levels/TutorialBlockOnSlope.js
@@ -9,7 +9,7 @@ Levels.TutorialBlockOnSlope = function()
 	this.sampleSolution = "var dt = .02; // sample time \nvar sum_pos_error = 0; // approximates integral of error \nfunction controlFunction(block)\n{\n  // Start by finding a proportional value that keeps the block on the screen \n  // Next add a derivative term that has a response with a few oscillations \n // Finally add a 3rd integral term to eliminate the steady state error  \n// Keep the gains from each step \n \n  sum_pos_error += block.x*dt;  \n  \n  return 0;\n}";
 	this.boilerPlateCode = "var dt = .02; // sample time\nfunction controlFunction(block)\n{\n  return 0;\n}";
 	this.description = "Push the block under the arrow (x=0) and make it stop there. Calculate the horizontal force on the block necessary to achieve this. This time the block is on a slope. Proportional or PD control alone will not work.";
-	this.model = new Models.BlockOnSlope({g: 5,x: -2,dx: 0,slope: -0.4,friction: 0});
+	this.model = new Models.BlockOnSlope({g: 5,x: -2,dx: 0,slope: -0.4,friction: 0.5});
 }
 
 
